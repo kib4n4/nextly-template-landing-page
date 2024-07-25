2d91d206fd82f94d0260258afcd369e600ba81a8
Luther Isaboke <lusaboke@gmail.com>
2024-07-25 20:43:40 +0300

    commited to modifying or landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,12 +13,10 @@ export default function Home() {
     <Container>
       <Hero />
       <SectionTitle
-        preTitle="Nextly Benefits"
-        title=" Why should you use this landing page"
+       
+        title=" PODstartSTudios"
       >
-        Nextly is a free landing page & marketing website template for startups
-        and indie projects. Its built with Next.js & TailwindCSS. And its
-        completely open-source.
+      welcome to our Studios' landing page
       </SectionTitle>
 
       <Benefits data={benefitOne} />
